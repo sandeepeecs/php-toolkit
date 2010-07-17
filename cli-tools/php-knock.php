80a41d3597641e9e98144a9cdacae190d92e9719
tomaszsobczak <tomaszsobczak@jabster.pl>
2010-07-17 21:36:14 +0200

    cosmetic changes

--- cli-tools/php-knock.php
+++ cli-tools/php-knock.php
@@ -1,12 +1,21 @@
 <?php
 
-  //  usage php-knock.php   HOSTNAME  PORT1 PORT2 PORT3 ...
+/*
+  Simple Port knocker implementation
 
-  foreach(array_slice($argv, 2) as $dst_port)
-  {
-    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-              @socket_connect($socket, $argv[1], $dst_port);
-              socket_close($socket);
-  }
+  usage:
+    php-knock.php   HOSTNAME  PORT1 PORT2 PORT3 ...
+
+  @author     Tomasz Sobczak (http://tomaszsobczak.com)
+  @package    PHP Toolkit (http://github.com/tomaszsobczak/php-toolkit)
+  @subpackage cli-tools
+*/
+
+foreach(array_slice($argv, 2) as $dst_port)
+{
+  $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+            @socket_connect($socket, $argv[1], $dst_port);
+            socket_close($socket);
+}
 
 ?>
